a3d3e926ed5090cf7add821ee993338a3a206f53
ÖMER KISAKOL <okisakol@gmail.com>
2024-11-15 16:30:29 +0300

    feat(shopping cart): implement shopping cart page and responsive design

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,21 +1,19 @@
 module.exports = {
     siteUrl: 'https://product-e-commerce-omerkisakol.vercel.app',
     generateRobotsTxt: true,
-    generateRobotsTxt: true,
     changefreq: 'daily',
     priority: 0.7,
-    exclude: [],
     sitemapSize: 5000,
     transform: async (config, path) => {
         return {
-            loc: path, // URL
+            loc: path,
             changefreq: config.changefreq,
             priority: path === '/' ? 1 : 0.7,
             lastmod: new Date().toISOString(),
         };
     },
     additionalPaths: async (config) => {
-        const productSlugs = ['product1', 'product2', 'product3'];
+        const productSlugs = ['1', '2', '3'];
         return productSlugs.map((slug) => ({
             loc: `/product?id=${slug}`,
             changefreq: 'weekly',
